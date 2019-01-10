45b61ba43717b47fc318b658146b1180b1fdd2c5
Tyrone Valmores <johntyronevalmores@gmail.com>
2019-01-10 01:09:02 -0800

    'test-to-see-if-it-will-update'

--- src/Components/footer.jsx
+++ src/Components/footer.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Grid, Row, Col, Image } from 'react-bootstrap';
+import { Grid, Row, Image } from 'react-bootstrap';
 
 class Footer extends Component {
     render() {
--- src/Components/header.jsx
+++ src/Components/header.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
-import { Jumbotron, Grid, Row, Col, Image, Button } from 'react-bootstrap';
+import { Jumbotron, Grid, Image } from 'react-bootstrap';
 
 
 class Header extends Component {
