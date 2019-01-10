60f5691e61cc25ad0da1a1e603f55eeaa1b8d339
Tyrone Valmores <johntyronevalmores@gmail.com>
2019-01-10 03:06:23 -0800

    Updates

--- precache-manifest.54f47e90997a71e0d45ecaf747cbd764.js
+++ precache-manifest.54f47e90997a71e0d45ecaf747cbd764.js
@@ -1,15 +1,15 @@
 self.__precacheManifest = [
   {
-    "revision": "daf5464e6fd44f7bae5581c8891aadab",
-    "url": "/tyronevalmores/static/media/work3.daf5464e.jpg"
+    "revision": "bfbb52ff2a1ef5e5a0e16e2b76702f88",
+    "url": "/tyronevalmores/static/media/work2.bfbb52ff.jpg"
   },
   {
-    "revision": "a2d6462b957f853c2e59",
-    "url": "/tyronevalmores/static/css/main.a7ed1b1a.chunk.css"
+    "revision": "6e1fde5c377a16100520",
+    "url": "/tyronevalmores/static/css/main.2ce516e6.chunk.css"
   },
   {
-    "revision": "bacd218c09df72131cf0",
-    "url": "/tyronevalmores/static/js/1.bacd218c.chunk.js"
+    "revision": "e028b306f405a251f4fe",
+    "url": "/tyronevalmores/static/js/1.e028b306.chunk.js"
   },
   {
     "revision": "29ccf80889a8444972b5",
@@ -24,16 +24,16 @@ self.__precacheManifest = [
     "url": "/tyronevalmores/static/media/work1.a1f6f698.jpg"
   },
   {
-    "revision": "a2d6462b957f853c2e59",
-    "url": "/tyronevalmores/static/js/main.a2d6462b.chunk.js"
-  },
-  {
-    "revision": "bfbb52ff2a1ef5e5a0e16e2b76702f88",
-    "url": "/tyronevalmores/static/media/work2.bfbb52ff.jpg"
+    "revision": "6e1fde5c377a16100520",
+    "url": "/tyronevalmores/static/js/main.6e1fde5c.chunk.js"
   },
   {
     "revision": "95f2130c72849fe26ee5672ee0b58e11",
     "url": "/tyronevalmores/static/media/work5.95f2130c.jpg"
+  },
+  {
+    "revision": "daf5464e6fd44f7bae5581c8891aadab",
+    "url": "/tyronevalmores/static/media/work3.daf5464e.jpg"
   },
   {
     "revision": "3e0ef42da892a6a436e8f0bb720e5cdc",
@@ -44,7 +44,7 @@ self.__precacheManifest = [
     "url": "/tyronevalmores/static/media/work6.c428ba0c.png"
   },
   {
-    "revision": "930b69d58bde84e0dc34accc83816f9e",
+    "revision": "7ba13c3001b9c7678a4cf4fc177a52d1",
     "url": "/tyronevalmores/index.html"
   }
 ];
